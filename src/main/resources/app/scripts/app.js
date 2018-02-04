d31d94f8f2a9c223b62f8fa648796ed8676b8497
Anick-Marie Bouchard <perilisk@localhost.localdomain>
2018-02-04 15:14:40 +0100

    using bootstrap's cards deck
    still in WIP, because cannot manage to get automatically to the new
    line after n cards.

--- src/main/resources/app/scripts/app.js
+++ src/main/resources/app/scripts/app.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/helloworld';
 import PizzaComponent from './components/pizza';
 
-
-ReactDOM.render(<HelloWorld/>, document.body);
-ReactDOM.render(<PizzaComponent/>, document.body);
+ReactDOM.render(<PizzaComponent/>, document.getElementById('pizzas'));
--- src/main/resources/app/scripts/components/pizza.js
+++ src/main/resources/app/scripts/components/pizza.js
@@ -20,14 +20,24 @@ class PizzaComponent extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Liste des pizzas</h1>
-        <ul>
-          {this.state.pizzas.map(p =>
-               <li><b>{p.Name}</b> <i>{p.Desc}</i> <b>{p.price} Eur</b></li>
-          )}
-        </ul>
-      </div>
+          <div>
+    		  <h1>Liste des pizzas</h1>
+    		    <div className="container-fluid">
+    		      <div className="card-deck">
+          {this.state.pizzas.map((p, i) =>
+             <div className="card" styles={{width : '20rem'}}>
+             
+                <div className="card-block">
+                  <h4 className="card-title">{p.Name}</h4>
+                  <p className="card-text">{p.Desc}</p>
+                  <a href='#' className="btn btn-primary">Commander</a>
+                </div>
+              </div>
+          )
+          }
+                </div>
+              </div>
+          </div>
     );
   }
 }
